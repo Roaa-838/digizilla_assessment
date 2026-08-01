88f6392642b999e4e6174820b7e468f7c44fbc5b
roaa-838 <roaaraafatzahran2003@gmail.com>
2026-08-01 14:16:21 +0300

    Hide Create button on form view

---
+++ addons/digizilla/static/src/js/digizilla_form.js
@@ -0,0 +1,13 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { FormController } from "@web/views/form/form_controller";
+
+patch(FormController.prototype, {
+    get canCreate() {
+        if (this.props.resModel === "digizilla.digizilla") {
+            return false;
+        }
+        return super.canCreate;
+    },
+});
