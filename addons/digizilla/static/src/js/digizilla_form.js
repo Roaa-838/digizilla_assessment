9a880137793a99a5ee5a4e06afb4b882f2042df5
roaa-838 <roaaraafatzahran2003@gmail.com>
2026-08-01 16:20:47 +0300

    Owl-based approach

--- addons/digizilla/static/src/js/digizilla_form.js
+++ addons/digizilla/static/src/js/digizilla_form.js
@@ -1,13 +1,16 @@
-/** @odoo-module **/
+// /** @odoo-module **/
 
-import { patch } from "@web/core/utils/patch";
-import { FormController } from "@web/views/form/form_controller";
+// import { patch } from "@web/core/utils/patch";
+// import { FormController } from "@web/views/form/form_controller";
 
-patch(FormController.prototype, {
-    get canCreate() {
-        if (this.props.resModel === "digizilla.digizilla") {
-            return false;
-        }
-        return super.canCreate;
-    },
-});
+// patch(FormController.prototype, {
+//     get canCreate() {
+//         if (this.props.resModel === "digizilla.digizilla") {
+//             return false;
+//         }
+//         return super.canCreate;
+//     },
+//     set canCreate(value) {
+       
+//     },
+// });
